0a0568a76278a90726cacf6c9f5336b1e29e4187
MariapBouAoun <73455465+MariaPBouAoun3@users.noreply.github.com>
2021-06-25 11:28:33 +0300

    Update PilePleineException.java

--- tp3/question1/PilePleineException.java
+++ tp3/question1/PilePleineException.java
@@ -3,8 +3,8 @@ package question1;
 /**
  * Décrivez votre classe PilePleineException ici.
  * 
- * @author (votre nom)
- * @version (un numéro de version ou une date)
+ * @author Maria Bou Aoun
+ * @version 2.0
  */
 public class PilePleineException extends Exception {
 }
