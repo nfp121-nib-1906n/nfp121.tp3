5c30dc6434d9c46d72bc7b5477aed6217e16e274
MariapBouAoun <73455465+MariaPBouAoun3@users.noreply.github.com>
2021-06-25 13:06:54 +0300

    Update UneUtilisation.java

--- tp3/question3/UneUtilisation.java
+++ tp3/question3/UneUtilisation.java
@@ -5,8 +5,8 @@ import question1.PolygoneRegulier;
 public class UneUtilisation {
 
     public static void main(String[] args) throws Exception {
-        // déclarer p1
-        // déclarer p2
+        PileI<PolygoneRegulier> p1 = new Pile2 (10);
+        PileI<PileI<PolygoneRegulier>> p2 = new Pile2 (10);
 
         // p1 est ici une pile de polygones réguliers PolygoneRegulier.java
         p1.empiler(new PolygoneRegulier(4, 100));
